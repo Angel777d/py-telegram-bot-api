ffe632776d5882af927d89c787e0e1abf5b4f21f
Angelovich <angel777da@gmail.com>
2021-02-27 17:57:34 +0300

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="py-telegram-bot-api",
-    version="0.0.6",
+    version="0.0.7",
     description="Simple, one file, zero dependency Telegram bot api wrapper",
     long_description=README,
     long_description_content_type="text/markdown",
